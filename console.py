79b324e5f80913fc5f7cda7cd8333fcfdc2de2e6
John Ross <johnyboy11@hotmail.co.uk>
2021-04-23 16:36:18 +0100

    classes populated

--- console.py
+++ console.py
@@ -5,3 +5,4 @@ from models.system import System
 
 import repositories.visit_repository as visit_repository
 import repositories.system_repository as system_repository
+
---
+++ models/visit.py
@@ -0,0 +1,10 @@
+class Visit:
+
+    def __init__(self, goal, system, achieved = False, id  = None):
+        self.goal = goal
+        self.system = system
+        self.achieved = achieved
+        self.id = id
+
+    def mark_achieved(self):
+        self.completed = True
