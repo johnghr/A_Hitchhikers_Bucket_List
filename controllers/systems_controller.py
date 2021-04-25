f724550bc11fdb2e6abf1421cb430887fc2b0afe
John Ross <johnyboy11@hotmail.co.uk>
2021-04-25 15:51:28 +0100

    systems form updating database and result displaying on page

--- controllers/systems_controller.py
+++ controllers/systems_controller.py
@@ -11,3 +11,15 @@ systems_blueprint = Blueprint("systems", __name__)
 def systems():
     systems = system_repository.select_all()
     return render_template('systems/index.html', systems = systems)
+
+@systems_blueprint.route("/systems/new", methods=['GET'])
+def new_system():
+    systems = system_repository.select_all()
+    return render_template("visits/new.html", all_systems = systems)
+
+@systems_blueprint.route("/systems", methods=['POST'])
+def create_system():
+    name = request.form['system']
+    system = System(name) 
+    system_repository.save(system)
+    return redirect('/systems')
