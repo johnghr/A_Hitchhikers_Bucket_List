63654fa75fcdbf0d111c4b2520648bbd8aad984a
John Ross <johnyboy11@hotmail.co.uk>
2021-04-25 16:09:59 +0100

    planets added to tables

--- db/hitchhike_manager.sql
+++ db/hitchhike_manager.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS visits;
+DROP Table IF EXISTS planets;
 DROP TABLE IF EXISTS systems;
-
 
 
 
@@ -9,9 +9,16 @@ CREATE TABLE systems (
     name VARCHAR(255)
 );
 
+CREATE TABLE planets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    system_id INT REFERENCES system(id)
+);
+
 CREATE TABLE visits (
     id SERIAL PRIMARY KEY,
     goal VARCHAR(255)
     system_id INT REFERENCES systems(id),
+    planet_id INT REFERENCES planets(id)
     achieved BOOLEAN 
 );
